a43e5de23f70612a45d6935f507a3bbd0e4ccb01
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2022-01-23 12:20:30 -0500

    now properly logging in console when quotes failed to load

--- src/main/java/io/github/seriousguy888/cheezsurvquotes/CheezSurvQuotes.java
+++ src/main/java/io/github/seriousguy888/cheezsurvquotes/CheezSurvQuotes.java
@@ -5,6 +5,7 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -33,6 +34,8 @@ public final class CheezSurvQuotes extends JavaPlugin {
     dataManager.loadPlayerData();
 
     getQuotes();
+    if(!getServer().getPluginManager().isPluginEnabled(this))
+      return;
 
     Bukkit.getPluginManager().registerEvents(dataManager, this);
     Objects.requireNonNull(getCommand("togglequotes"))
@@ -49,7 +52,7 @@ public final class CheezSurvQuotes extends JavaPlugin {
     String urlString = config.getString("quotes.source_url");
     String delimiter = config.getString("quotes.delimiter");
     if(urlString == null || delimiter == null) {
-      getLogger().warning("Missing or invalid config options!");
+      getLogger().severe("Missing or invalid config options!");
       getServer().getPluginManager().disablePlugin(this);
       return;
     }
@@ -58,7 +61,7 @@ public final class CheezSurvQuotes extends JavaPlugin {
     try {
       uri = URI.create(urlString);
     } catch (IllegalArgumentException e) {
-      getLogger().warning("Could not load quotes because of invalid URL.\n" + e);
+      getLogger().severe("Could not load quotes because of invalid URL.\n" + e);
       getServer().getPluginManager().disablePlugin(this);
       return;
     }
@@ -72,17 +75,24 @@ public final class CheezSurvQuotes extends JavaPlugin {
         .uri(uri)
         .GET()
         .build();
-    client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
-        .thenAccept(response -> {
-          if(response.statusCode() != 200) {
-            getLogger().warning("Failed to load quotes. HTTP request response code was " + response.statusCode() + " and not 200.");
-            getServer().getPluginManager().disablePlugin(this);
-            return;
-          }
 
-          String body = response.body();
-          quotesList = Arrays.asList((body.split(delimiter)));
-          new ShowQuoteTask(this).runTaskTimer(this, 0L, interval * 20L);
-        });
+    try {
+      HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+      if(response.statusCode() != 200) {
+        getLogger().severe("Failed to load quotes. HTTP request response code was " +
+            response.statusCode() + " and not 200.");
+        getServer().getPluginManager().disablePlugin(this);
+        return;
+      }
+
+      String body = response.body();
+      quotesList = Arrays.asList((body.split(delimiter)));
+      new ShowQuoteTask(this).runTaskTimer(this, 0L, interval * 20L);
+
+    } catch (IOException | InterruptedException e) {
+      getLogger().severe("Failed to load quotes because HTTP request failed. Make sure that the source URL is set correctly in the plugin config. " + e);
+      getServer().getPluginManager().disablePlugin(this);
+    }
   }
 }
